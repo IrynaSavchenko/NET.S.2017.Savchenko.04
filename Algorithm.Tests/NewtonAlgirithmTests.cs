53103b0f721ba07a56528fd5310b57d4faead28a
phoenixIS <savchira9@gmail.com>
2017-07-06 02:59:03 +0300

    Add project files.

---
+++ Algorithm.Tests/NewtonAlgirithmTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Algorithm.Tests
+{
+    [TestFixture]
+    public class NewtonAlgorithmTests
+    {
+        [TestCase(16, 4, 0.00001)]
+        [TestCase(10.345, 26, 1E-16)]
+        [TestCase(-9, 3, 0.001)]
+        [TestCase(-634517, 5, 0.00001)]
+        [TestCase(-45.5, 11, 1E-10)]
+        public void SqrtN_ValidInputParameters_NthRootWithPercisionReturned(double number, double n, double epsilon)
+        {
+            double calculatedValue = NewtonAlgorithm.SqrtN(number, n, epsilon);
+            double exactValue = Math.Pow(number, 1.0 / n);
+
+            Assert.Less(Math.Abs(exactValue - calculatedValue), epsilon);
+        }
+
+        [TestCase(15, -4, 0.0001)]
+        [TestCase(15.7, 0, 0.0001)]
+        [TestCase(-19, 8, 0.00001)]
+        [TestCase(15, 7, -0.001)]
+        [TestCase(9.2, 3, 1E-600)]
+        public void SqrtN_InvalidParameters_NthRootCannotBeTaken(double number, double n, double epsilon)
+        {
+            Assert.Throws<ArgumentException>(() => NewtonAlgorithm.SqrtN(number, n, epsilon));
+        }
+    }
+}
---
+++ Algorithm/NewtonAlgorithm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// Realization of the Newton algorithm
+    /// </summary>
+    public static class NewtonAlgorithm
+    {
+        private const double MinPrecision = 0.1;
+        private static readonly double MaxPrecision = Math.Pow(10, -sizeof(double) * 8);
+
+        /// <summary>
+        /// Finds the nth root of the given number using the Newton algorithm
+        /// </summary>
+        /// <param name="number">Number to extract root from</param>
+        /// <param name="n">Root degree</param>
+        /// <param name="epsilon">Precision</param>
+        /// <returns>The nth root of the given number</returns>
+        public static double SqrtN(double number, double n, double epsilon = 0.0001)
+        {
+            CheckParameters(number, n, epsilon);
+
+            double currentValue;
+            double nextValue = number / n;
+
+            do
+            {
+                currentValue = nextValue;
+                nextValue = GetNextValue(currentValue, number, n);
+            } while (Math.Abs(nextValue - currentValue) > epsilon);
+
+            return nextValue;
+        }
+
+        /// <summary>
+        /// Performs the next iteration of the Newton algorithm and returns its result
+        /// </summary>
+        /// <param name="currentValue">Approximation calculated at previous iterations</param>
+        /// <param name="number">Number to extract root from</param>
+        /// <param name="n">Root degree</param>
+        /// <returns></returns>
+        private static double GetNextValue(double currentValue, double number, double n)
+        {
+            return ((n -1 ) * currentValue + number / Math.Pow(currentValue, (n - 1))) / n;
+        }
+
+        /// <summary>
+        /// Checks if possible to find the nth root with the given parameters
+        /// </summary>
+        /// <param name="number">Number to extract root from</param>
+        /// <param name="n">Root degree</param>
+        /// <param name="epsilon">Precision</param>
+        private static void CheckParameters(double number, double n, double epsilon)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException($"Root degree {nameof(n)} cannot be negative");
+            }
+            if (epsilon > MinPrecision || epsilon < MaxPrecision)
+            {
+                throw new ArgumentException($"Precision value {nameof(epsilon)} must be positive");
+            }
+            if (number < 0 && n % 2 == 0)
+            {
+                throw new ArgumentException($"Aliquot root {n} cannot be taken from a negative number {number}");
+            }
+        }
+    }
+}
